e1cf7b74377a44ec96c3b6d167804c20b42cb028
lucasimondi <luca.simondi@gmail.com>
2026-06-25 15:53:18 +0200

    Rimuovi dist locale, Vercel compila da sorgente

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+export { default as Ic } from './Ic.jsx';
+export { default as Btn } from './Btn.jsx';
+export { default as Modal } from './Modal.jsx';
+export { default as Toast } from './Toast.jsx';
+export { default as PhStr } from './PhStr.jsx';
+export { Bdg, Crd, Fld } from './atoms.jsx';
+export { Inp, Sel, Txt } from './inputs.jsx';
+
+export { default as SearchSel } from './SearchSel.jsx';
